39cbe16dbf55688a6960529dd82223da14c7f1fe
Sasha Pimenov <pimenovs33@gmail.com>
2025-11-15 18:49:38 +0200

    Add storage.service; save token

--- weather.js
+++ weather.js
@@ -1,6 +1,17 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
 import { printError, printHelp, printSuccess } from './services/log.service.js';
+import { saveKeyValue } from "./services/storage.service.js";
+
+const saveToken = async (token) => {
+	try {
+		await saveKeyValue('token', token);
+		printSuccess('Token saved successfully');
+	} catch (err) {
+		printError(err.message);
+	}
+};
+
 
 const initCLI = () => {
 	const args = getArgs(process.argv);
@@ -15,6 +26,7 @@ const initCLI = () => {
 
 	if (args.t) {
 		// Set token
+		saveToken(args.t);
 	}
 
 	// Fetch and display weather
